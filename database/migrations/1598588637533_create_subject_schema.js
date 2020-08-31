a046ef8bd18664fcf44c21127bff604ec331217f
devillkaow <devillkaow@gmail.com>
2020-08-31 21:10:33 +0700

    first commit

---
+++ database/migrations/1598588637533_create_subject_schema.js
@@ -0,0 +1,27 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class CreateSubjectSchema extends Schema {
+  up () {
+    this.create('subjects', (table) => {
+      table.increments('subject_id')
+      table.string('title', 120).unique().notNullable()
+      table.integer('teacher_id').unsigned().notNullable()
+      table.timestamps()
+
+      table
+      .foreign('teacher_id')
+      .references('teachers.teacher_id')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE')
+    })
+  }
+
+  down () {
+    this.drop('subjects')
+  }
+}
+
+module.exports = CreateSubjectSchema
